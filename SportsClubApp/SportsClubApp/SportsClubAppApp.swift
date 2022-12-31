4d70d2c2204e4a86a164a82be5fb23e7bc94f397
Engin Gülek <engingulek0@gmail.com>
2022-12-31 19:39:42 +0300

    Create Project

---
+++ SportsClubApp/SportsClubApp/SportsClubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsClubAppApp.swift
+//  SportsClubApp
+//
+//  Created by engin gülek on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SportsClubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
